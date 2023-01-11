1672d97e52ddee215a7c019ed1246970d22c7663
PleaBanshee <llewellynant@gmail.com>
2023-01-11 15:02:40 +0200

    Added product route

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const dotenv = require('dotenv');
 
 // Configs
-dotenv.config({path: './config/config.env'});
+dotenv.config({path: 'backend/config/config.env'});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
