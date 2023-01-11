767c14e7f358d6a845bcb9b4cfa64740a181282d
PleaBanshee <llewellynant@gmail.com>
2023-01-11 23:04:19 +0200

    Created route for adding new products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,14 @@
+const Product = require('../models/Product');
+
+// create a new product --- POST /api/v1/admin/product/new
+exports.newProduct = async (req,res,next) => {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
 exports.getProducts = (req,res,next) => {
     res.status(200).json({
         success: true,
