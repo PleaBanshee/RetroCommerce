bcc0a01b3e0416066e9f36f19c709bb827f08194
PleaBanshee <llewellynant@gmail.com>
2023-01-11 23:04:19 +0200

    Created route for adding new products

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    // name of producr
+    // name of product
     name: {
         type: String,
         required: [true, "Please enter product name"],
