d8733ab50380cdd18b001c750a316b4281ea9e6f
PleaBanshee <llewellynant@gmail.com>
2023-01-12 08:54:02 +0200

    Added code to return all products for products endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -9,9 +9,14 @@ exports.newProduct = async (req,res,next) => {
     })
 }
 
-exports.getProducts = (req,res,next) => {
+// retrieve all products --- GET /api/v1/products
+exports.getProducts = async (req,res,next) => {
+    const products = await Product.find();
+
     res.status(200).json({
         success: true,
-        message: "This route will show all products in database"
+        message: "This route displays all products in database",
+        count: products.length,
+        products
     })
 }
