0370f9d9c7689e483cd99c249220a35b642e5e08
PleaBanshee <llewellynant@gmail.com>
2023-01-20 08:17:54 +0200

    Added code to handle unhandled promise rejections

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 const app = require("./app");
-const connectDB = require('./config/database');
-const dotenv = require('dotenv');
+const connectDB = require("./config/database");
+const dotenv = require("dotenv");
 
 // Configs
-dotenv.config({path: 'backend/config/config.env'});
+dotenv.config({ path: "backend/config/config.env" });
 connectDB();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
+const server = app.listen(process.env.PORT, () => {
+    console.log(
+        `Server is running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+    );
+});
+
+// handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+    console.log(`ERROR: ${err.message}`);
+    console.log("Shutting down the server due to unhandled promises");
+    server.close(() => {
+        process.exit(1);
+    });
 });
