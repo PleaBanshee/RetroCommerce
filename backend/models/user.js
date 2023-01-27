b06d447a4a81d062c0cf6766aff6fca073fff72c
PleaBanshee <llewellynant@gmail.com>
2023-01-27 14:33:52 +0200

    Generating forgot password token

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -62,6 +63,21 @@ userSchema.methods.getJwtToken = function() {
     return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_TIME,
     });
+};
+
+// generate password reset token
+userSchema.methods.getresetpasswordToken = function() {
+    const resetToken = crypto.randomBytes(20).toString("hex");
+
+    // hash token and reset token
+    this.resetPasswordToken = crypto
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex");
+
+    // set token expire time
+    this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+    return resetToken;
 };
 
 module.exports = mongoose.model("User", userSchema);
