0b1822f95afe7322761b4c1157dd1f49e42e1abf
PleaBanshee <llewellynant@gmail.com>
2023-01-27 15:26:11 +0200

    Sending emails for password recoveries

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require("../models/user");
 
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
-const { json } = require("body-parser");
+const sendEmail = require("../utils/sendEmail");
 const sendToken = require("../utils/jwtToken");
 
 // Register a user => /api/v1/register
@@ -47,6 +47,46 @@ exports.loginUser = catchAsyncErrors(async(req, res, next) => {
     const token = user.getJwtToken();
 
     sendToken(user, 200, res);
+});
+
+// forgot password => /api/v1/password/forgot
+exports.forgotPassword = catchAsyncErrors(async(req, res, next) => {
+    const user = await User.findOne({
+        email: req.body.email,
+    });
+
+    if (!user) {
+        return new ErrorHandler("Invalid email or password", 404);
+    }
+
+    // get reset token for password
+    const resetToken = user.getResetPasswordToken();
+    await user.save({ validateBeforeSave: false });
+
+    // create reset password URL
+    const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/password/reset/${resetToken}`;
+
+    const message = `Your password reset token:\n\n${resetUrl}\n\nIf you have not requested this email, then ignore it.`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: "RetroCommerce password reset",
+            message,
+        });
+
+        res.status(200).json({
+            success: true,
+            message: `Email sent to ${user.email}`,
+        });
+    } catch (err) {
+        user.getResetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+        await user.save({ validateBeforeSave: false });
+        return next(new ErrorHandler(err.message, 500));
+    }
 });
 
 // Log out a user and clear cookie -> /api/v1/logout
