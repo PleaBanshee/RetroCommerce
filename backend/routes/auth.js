a9e0500323ff395e8d9809cc5695ea3396d3ce0c
PleaBanshee <llewellynant@gmail.com>
2023-01-30 10:10:19 +0200

    Adding routes to view all users and get user by id as an admin

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,16 +7,24 @@ const {
     logout,
     forgotPassword,
     resetPassword,
-    getuserProfile,
+    getUserProfile,
     updatePassword,
     updateProfile,
+    allUsers,
+    getUserDetails,
 } = require("../controllers/authController");
 
-const { isAuthenticatedUser } = require("../middlewares/auth");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/me").get(isAuthenticatedUser, getuserProfile);
+router
+    .route("/admin/users")
+    .get(isAuthenticatedUser, authorizeRoles("admin"), allUsers);
+router
+    .route("/admin/user/:id")
+    .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails);
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 router.route("/me/update").put(isAuthenticatedUser, updateProfile);
 router.route("/password/forgot").post(forgotPassword);
