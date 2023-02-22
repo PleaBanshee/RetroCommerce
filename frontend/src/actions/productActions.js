820a4bf79edd926b96dd766e39ec6547a1a26dc0
Plea_Banshee <69715891+PleaBanshee@users.noreply.github.com>
2023-02-22 11:08:34 +0200

    Creating Product details reducer and actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,6 +4,9 @@ import {
   ALL_PRODUCTS_SUCCESS,
   ALL_PRODUCTS_REQUEST,
   CLEAR_ERRORS,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants";
 
 export const getProducts = () => async (dispatch) => {
@@ -19,6 +22,27 @@ export const getProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: ALL_PRODUCTS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const getProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/v1/product/${id}`);
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data.product,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
