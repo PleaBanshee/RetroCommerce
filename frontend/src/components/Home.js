b7bcde47e64540e8fb533a4f1d1fe3e38dd703a8
LlewellynAnthony <lanthony@retrorabbit.co.za>
2023-02-27 15:37:51 +0200

    Creating slider to filter products by price

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Fragment, useEffect, useState } from "react";
 import MetaData from "./layout/MetaData";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 import { useDispatch, useSelector } from "react-redux";
 import Product from "./product/Product";
 import { getProducts } from "../actions/productActions";
@@ -8,11 +10,15 @@ import { useAlert } from "react-alert";
 import Pagination from "react-js-pagination";
 import { useParams } from "react-router-dom";
 
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
+
 const Home = () => {
   const alert = useAlert();
   const dispatch = useDispatch();
-  const [currentPage, setCurrentPage] = useState(1);
   const params = useParams();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([1, 1000]);
 
   const {
     loading,
@@ -30,8 +36,8 @@ const Home = () => {
       return alert.error(error);
     }
 
-    dispatch(getProducts(keyword, currentPage));
-  }, [dispatch, alert, error, keyword, currentPage]);
+    dispatch(getProducts(keyword, currentPage, price));
+  }, [dispatch, alert, error, keyword, currentPage, price]);
 
   function setCurrentPageNo(pageNumber) {
     setCurrentPage(pageNumber);
@@ -47,10 +53,47 @@ const Home = () => {
           <h1 id="products_heading">Latest Products</h1>
           <section id="products" className="container mt-5">
             <div className="row">
-              {products &&
+              {keyword ? (
+                <Fragment>
+                  <div className="col-6 col-md-3 mt-5 mb-5">
+                    <div className="px-5">
+                      <Range
+                        marks={{
+                          1: `$1`,
+                          1000: `$1000`,
+                        }}
+                        min={1}
+                        max={1000}
+                        defaultValue={[1, 1000]}
+                        tipFormatter={(value) => `$${value}`}
+                        tipProps={{
+                          placement: "top",
+                          visible: true,
+                        }}
+                        value={price}
+                        onChange={(price) => setPrice(price)}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-6 col-md-9">
+                    <div className="row">
+                      {products &&
+                        products.map((product) => (
+                          <Product
+                            key={product._id}
+                            product={product}
+                            col={4}
+                          />
+                        ))}
+                    </div>
+                  </div>
+                </Fragment>
+              ) : (
+                products &&
                 products.map((product) => (
                   <Product key={product._id} product={product} col={3} />
-                ))}
+                ))
+              )}
             </div>
           </section>
           {resPerPage <= filteredProductsCount && (
