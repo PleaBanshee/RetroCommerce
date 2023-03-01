38fc35a42e5d2c7bab615ec2ebd78d5354d526fa
LlewellynAnthony <lanthony@retrorabbit.co.za>
2023-03-01 13:53:08 +0200

    Adding Cloudinary setups to project

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const bodyparser = require("body-parser");
+const cloudinary = require("cloudinary");
 
 const errorMiddleware = require("./middlewares/errors");
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// Cloudinary configs
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // Import all the routes
 const products = require("./routes/product");
