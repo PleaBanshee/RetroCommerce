a0311a2d0957fa0882fc2b8e67488efb9d1d14a5
LlewellynAnthony <lanthony@retrorabbit.co.za>
2023-03-02 16:47:02 +0200

    Fixing registering component and allowing larger profile image uploads

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
-const bodyparser = require("body-parser");
-const cloudinary = require("cloudinary");
+const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 
 const errorMiddleware = require("./middlewares/errors");
-app.use(express.json());
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 app.use(cookieParser());
 app.use(fileUpload());
-
-// Cloudinary configs
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 // Import all the routes
 const products = require("./routes/product");
