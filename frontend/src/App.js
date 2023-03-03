1494342ee0c48c54426e36a63d491d573d523068
LlewellynAnthony <lanthony@retrorabbit.co.za>
2023-03-03 13:32:08 +0200

    Adding route protection to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import ProductDetails from "./components/product/ProductDetails";
 import Login from "./components/user/Login";
 import Register from "./components/user/Register";
 import Profile from "./components/user/Profile";
+import ProtectedRoute from "./route/ProtectedRoute";
 import { loadUser } from "./actions/userActions";
 import store from "./store";
 import "./App.css";
@@ -27,7 +28,14 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/product/:id" element={<ProductDetails />} exact />
-            <Route path="/me" element={<Profile />} exact />
+            <Route
+              path="/me"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
         </div>
         <Footer />
